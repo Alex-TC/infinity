dbd6d39f00f1ad3347ecca0319c461b244e23ac8
jadenASC6 <jadenthakur14@gmail.com>
2019-11-28 15:39:23 -0500

    all around changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-// let box = document.body.querySelector('#box');
-// let logo = document.body.querySelector('#logo');
-// if (box.style.borderTop == "#cc0000 solid 1vh") {
-//     logo.style.top = "9.8vh";
-//     box.stlye.backgroundColor="white";
-// }
+let box = document.body.querySelector('#box');
+let logo = document.body.querySelector('#logo');
+let nav = document.body.querySelector('.nav');
+let center = document.body.querySelector('.center');
+let triangle = document.body.querySelector('#triangle');
+// if ()
+// triangle.style.display="none";
