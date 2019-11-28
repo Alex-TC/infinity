cef09de47016258c918496626994c9afd7bfbcce
jadenASC6 <jadenthakur14@gmail.com>
2019-11-27 22:49:42 -0500

    minor changes

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// let box = document.body.querySelector('#box');
+// let logo = document.body.querySelector('#logo');
+// if (box.style.borderTop == "#cc0000 solid 1vh") {
+//     logo.style.top = "9.8vh";
+//     box.stlye.backgroundColor="white";
+// }
